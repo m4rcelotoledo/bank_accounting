459a56ddc8af604de3047d520738fcce1570c5cb
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-03-15 15:26:23 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_172402) do
+ActiveRecord::Schema.define(version: 2019_05_21_205631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_05_04_172402) do
     t.float "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["account_id"], name: "index_transactions_on_account_id"
   end
 
