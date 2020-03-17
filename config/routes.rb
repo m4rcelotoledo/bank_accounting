8e4d67192e7b4daa561e2dd0a7d7b888288a6fa7
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-03-17 01:16:52 -0300

    Update routes.rb
    Routes refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users, only: %i[create index show] do
-    resources :accounts, only: %i[create show] do
-      resources :transactions, only: %i[create show]
-    end
-  end
+  resources :users, only: %i[create index show]
 
+  resources :accounts, only: %i[create show]
   get '/balance', to: 'accounts#balance'
+  get '/statement', to: 'accounts#statement'
+
+  resources :transactions, only: %i[index show]
+  post '/deposit', to: 'transactions#deposit'
   post '/transfer', to: 'transactions#transfer'
 end
