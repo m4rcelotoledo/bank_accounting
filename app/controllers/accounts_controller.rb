a0b687306c1f7f6ce95ed41d583b41592e185f59
Marcelo Toledo <marcelotoledo5000@gmail.com>
2025-07-27 22:51:03 -0300

    Reorganize files

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class AccountsController < ApplicationController
-  before_action :set_account, only: %i[show]
   before_action :validate_balance_params, only: [:balance]
   before_action :validate_statement_params, only: [:statement]
-
-  attr_reader :account
+  before_action :validate_user_exists_before_create, only: [:create]
 
   # GET /balance
   def balance
+    return if validate_account_exists(account_params[:account_id])
     Account.includes(:transactions).find(account_params[:account_id]).then do |account|
       account.current_balance.then { |balance| json_response({ balance: balance }) }
     end
@@ -20,15 +19,22 @@ class AccountsController < ApplicationController
       AccountService.balance_initial account.id
       json_response account, :created
     end
+  rescue ActiveRecord::RecordInvalid => e
+    render_unprocessable_entity(e.message)
   end
 
   # GET /accounts/:id
   def show
-    json_response @account
+    Account.includes(:user, :transactions).find(params[:id]).then do |account|
+      json_response account
+    end
+  rescue ActiveRecord::RecordNotFound
+    render_not_found("Couldn't find Account with 'id'=#{params[:id]}")
   end
 
   # GET /statement
   def statement
+    return if validate_account_exists(account_params[:account_id])
     Account.includes(:transactions).find(account_params[:account_id]).then do |account|
       account.transactions.then do |transactions|
         render json: transactions, status: :ok,
@@ -43,15 +49,15 @@ class AccountsController < ApplicationController
     params.permit(:user_id, :account_id)
   end
 
-  def set_account
-    @account = Account.includes(:user, :transactions).find params[:id]
-  end
-
   def validate_balance_params
     validate_presence_of_required_params(%i[account_id])
   end
 
   def validate_statement_params
     validate_presence_of_required_params(%i[account_id])
+  end
+
+  def validate_user_exists_before_create
+    validate_user_exists(account_params[:user_id])
   end
 end
